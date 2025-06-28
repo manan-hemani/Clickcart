62849d0c7993529d11e929ab3495952f8271c962
manan-hemani <mananhemani01@gmail.com>
2025-06-28 18:32:09 +0530

    backend

---
+++ backend/index.js
@@ -0,0 +1,136 @@
+const port = 4000;
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+const { error } = require("console");
+const { type } = require("os");
+
+app.use(express.json());
+app.use(cors());
+
+// DataBase Connection using MongoDB
+mongoose.connect(
+  "mongodb+srv://mananhemani:gems%241234%23@cluster0.n0yy0m7.mongodb.net/ClickCart"
+);
+
+// API Creation
+
+app.get("/", (req,res) => {
+    res.send("Express App is Running")    
+})
+
+// Image Storage
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req,file,cb) => {
+      return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+  }  
+})
+
+const upload = multer({ storage: storage })
+
+// Upload Product API
+app.use('/images',express.static('upload/images'))
+app.post("/upload", upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+// Schema for Creation of Products
+const Product = mongoose.model("Product", {
+    id: {
+        type: Number,
+        required:true,
+    },
+    name: {
+        type: String,
+        required:true,
+    },
+    image: {
+        type: String,
+        required:true,
+    },
+    category: {
+        type: String,
+        required:true,
+    },
+    new_price: {
+        type: Number,
+        required:true,
+    },
+    old_price: {
+        type: Number,
+        required:true,
+    },
+    date: {
+        type: Date,
+        default:Date.now,
+    },
+    available: {
+    type: Boolean,
+        default:true,
+    },
+})
+
+// Add Product API
+app.post('/addproduct', async (req, res) => {
+    let products = await Product.find({});
+    let id;
+    if (products.length>0) {
+        let latest_product_array = products.slice(-1);
+        let last_product = latest_product_array[0];
+        id = last_product.id + 1;
+    }
+    else {
+        id=1
+    }
+    const product = new Product({
+      id: id,
+      name: req.body.name,
+      image: req.body.image,
+      category: req.body.category,
+      new_price: req.body.new_price,
+      old_price: req.body.old_price,
+    });    
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success: true,
+        name: req.body.name,
+    })
+})
+
+// Remove Product API
+app.post('/removeproduct',async (req,res) => {
+    await Product.findOneAndDelete({ id: req.body.id })
+    console.log("Removed")
+    res.json({
+        success: true,
+        name:req.body.name      
+    })
+})
+
+// Retrieving All Products 
+app.get('/allproducts',async (req,res) => {
+    let products = await Product.find({});
+    console.log("Fetched All Products");
+    res.send(products);
+})
+
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log("Server Running on Port:"+port)
+    }
+    else {
+        console.log("ERROR: "+error)
+    }
+})
