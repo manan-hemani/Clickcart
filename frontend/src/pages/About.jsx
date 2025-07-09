8ffa6fdc839cabaae52acaa5ee632529b4b9c53d
manan-hemani <mananhemani01@gmail.com>
2025-07-09 14:15:45 +0530

    company details

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -11,11 +11,45 @@ const About = () => {
           <img src={about_image} alt="about us" width={500} />
         </div>
         <div className="about-right">
-          {" "}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
-          vitae nam suscipit pariatur distinctio impedit in repellendus sequi
-          nihil reiciendis praesentium ipsam, maxime eveniet sapiente cumque
-          numquam necessitatibus nemo illum.
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+            vitae nam suscipit pariatur distinctio impedit in repellendus sequi
+            nihil reiciendis praesentium ipsam, maxime eveniet sapiente cumque
+            numquam necessitatibus nemo illum.
+          </p>
+          <h3>Our Mission</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab error
+            delectus tempora, sit culpa ipsa possimus excepturi illum rem neque,
+            nulla expedita deleniti, quis inventore est totam perferendis vero
+            dignissimos?
+          </p>
+        </div>
+      </div>
+      <div className="about-why-chose-us">
+        <h2>Why Chose Us</h2>
+        <div className="about-why-format-main">
+          <div className="about-chose-details">
+            <h4>Quality Assuarance</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+              tempora quo iste.
+            </p>
+          </div>
+          <div className="about-chose-details">
+            <h4>Convinience</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+              tempora quo iste.
+            </p>
+          </div>
+          <div className="about-chose-details">
+            <h4>Customer Service</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+              tempora quo iste.
+            </p>
+          </div>
         </div>
       </div>
       <div className="admin-btn">
--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import "./CSS/ContactUs.css"
+import React from "react";
+import "./CSS/ContactUs.css";
+import contact_img from "../components/Assests/contact_us.jpg";
 
 const ContactUs = () => {
   return (
-    <div className='contact-us'>
-      
+    <div className="contact-us">
+      <h1>Contact Us</h1>
+      <div className="contact">
+        <div className="contact-left">
+          <img src={contact_img} alt="contactUs" width={500} />
+        </div>
+        <div className="contact-right">
+          <div className="contact-details">
+            <h3>Our Store</h3>
+            <p>
+              Balaji Ward, Jagdalpur <br />
+              Chhattisgarh, India <br />
+              494001
+            </p>
+            <br />
+            <p>Phone No.: +91 9876543210</p>
+            <p>
+              Email:{" "}
+              <a href="mailto:admin@clickcart.com">admin@clickcart.com</a>
+            </p>
+          </div>
+          <div className="contact-details">
+            <h3>Careers at ClickCart</h3>
+            <p>Learn more about our teams and job openings.</p>
+          </div>
+          <button className="jobs-btn">Explore Jobs</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactUs
+export default ContactUs;
